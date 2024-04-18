5c3004b944eed56f286c7034ac02ea9d2716137c
Olivier Halligon <olivier.halligon@automattic.com>
2024-04-18 14:31:08 +0200

    Fix overriding non-open class outside module

--- WordPressEditor/WordPressEditor/Classes/Plugins/WordPressPlugin/Gutenberg/Gutenblock.swift
+++ WordPressEditor/WordPressEditor/Classes/Plugins/WordPressPlugin/Gutenberg/Gutenblock.swift
@@ -10,7 +10,4 @@ class Gutenblock: ParagraphProperty {
     required public init?(coder aDecoder: NSCoder){
         super.init(coder: aDecoder)
     }
-
-    override public class var supportsSecureCoding: Bool { true }
-
 }
