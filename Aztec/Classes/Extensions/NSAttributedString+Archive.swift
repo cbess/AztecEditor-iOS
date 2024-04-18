90c0e4db1246dc4e87f004c4ec96061399447efb
Olivier Halligon <olivier.halligon@automattic.com>
2024-04-18 14:26:18 +0200

    Use `UTType.utf8PlainText` (explicit encoding) instead of `kUTTypePlainText`
    Since newer versions of iOS use this new `public.utf8-plain-text` UTI instead of `public.plain-text` like in previous OS versions.

--- Aztec/Classes/Extensions/NSAttributedString+Archive.swift
+++ Aztec/Classes/Extensions/NSAttributedString+Archive.swift
@@ -1,10 +1,11 @@
 import Foundation
+import UIKit
 
 // MARK: - NSAttributedString Archive methods
 //
 extension NSAttributedString
 {
-    static let pastesboardUTI = "com.wordpress.aztec.attributedString"
+    static let pasteboardUTI = UIPasteboard.UTType(identifier: "com.wordpress.aztec.attributedString")
 
     func archivedData() throws -> Data {
         return try NSKeyedArchiver.archivedData(withRootObject: self, requiringSecureCoding: false)
